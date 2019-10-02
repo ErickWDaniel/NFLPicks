e6eb26f39627ba48881919c27e9aaf8cb174dbb7
Prayson Wilfred Daniel <praysonwilfred@hotmail.com>
2019-10-02 14:52:10 +0200

    Update get_winners.py
    get wins for results page

--- nflpicks/utils/get_winners.py
+++ nflpicks/utils/get_winners.py
@@ -5,11 +5,13 @@ from nflpicks.utils.logs import logging
 
 
 def get_points():
-    
+
     try:
-        win = winners.Winners()
+        # 'mysite/nflpicks/data/data.sqlite'
+        win = winners.Winners('nflpicks/data/data.sqlite')
         s = win.data_cleaner.wins()
         points = s.points.sort_values('points', ascending=False)
+
     except Exception as k:
         logging.error(f'We ignored', exc_info=True)
         points = pd.DataFrame(
@@ -17,4 +19,21 @@ def get_points():
             'points': [128, 102, 42]})
     return points.to_dict(orient='records')
 
-    
+
+def get_points_per_round():
+
+    try:
+        win = winners.Winners('nflpicks/data/data.sqlite')
+        s = win.data_cleaner.wins()
+        points_per_round = s.points_per_round.sort_values('points', ascending=False)
+        
+    except Exception as k:
+        logging.error(f'We ignored', exc_info=True)
+        points_per_round = pd.DataFrame(
+            {'picker_id':['Johnny','Anne','Rachel'],
+            'points': [128, 102, 42],
+            'round': [1,1,1]})
+
+    return points_per_round.to_dict(orient='records')
+
+
